70be71f84aac1cf7c138bacf5a297e4949563eeb
alarosa <larosa2@rpi.edu>
2016-04-02 01:27:09 -0400

    Prototype board implemented, with piles of rocks and game logic.

---
+++ Assets/Scripts/Board/Board.cs
@@ -0,0 +1,152 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Board : MonoBehaviour {
+
+
+    public GameObject PileObject;
+    public GameObject[] piles;
+
+    public GameObject PlayerPile;
+
+    public int maxRocksPerTurn = 0;
+    public int PileLock = -1;
+
+    public string GameType = "none";
+    public int moveNum = 0;
+
+    public int rocksTakenThisTurn = 0;
+
+    public int dist = 0;
+
+    void Start()
+    {
+        startBasicNim();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab) || rocksTakenThisTurn == maxRocksPerTurn || checkEmptyPiles())
+        {
+            endTurn();
+        }
+    }
+
+    public void startBasicNim()
+    {
+        Debug.Log("GameType Started: Basic Nim");
+        GameType = "basic";
+        maxRocksPerTurn = 3;
+
+        PileLock = -1;
+
+        piles = new GameObject[1];
+        GameObject p = Instantiate(PileObject);
+        p.transform.parent = transform;
+
+        p.GetComponent<Pile>().GameType = GameType;
+
+        for (int i=0; i<11; i++)
+        {
+            p.GetComponent<Pile>().addRocks();
+        }
+
+        piles[0] = p;
+
+    }
+
+    public void startRowNim(int n)
+    {
+        maxRocksPerTurn = 1000000;
+        piles = new GameObject[n];
+
+        float rot = 60.0f * Mathf.Deg2Rad;
+
+        for (int i=0; i< n; i++)
+        {
+            GameObject p = Instantiate(PileObject);
+            p.transform.parent = transform;
+
+            float newx = p.transform.position.x + (dist * Mathf.Sin(rot));
+            float newy = p.transform.position.x + (dist * Mathf.Cos(rot));
+
+            p.transform.position = new Vector3(newx, newy, p.transform.position.z);
+
+            for (int j = 0; j < 5; j++)
+            {
+                p.GetComponent<Pile>().addRocks();
+            }
+
+            piles[i] = p;
+
+            rot += (120 * Mathf.Deg2Rad);
+        }
+    }
+
+    /* returns true if we can safely add to our pile*/
+    public bool pileCheck(Vector2 mc, GameObject p)
+    {
+        // Check if the box is overlapping, then if the pile is locked, then if we can take rocks
+        return PlayerPile.GetComponent<BoxCollider2D>().OverlapPoint(mc) && pileLockCheck(p) && maxRocksPerTurn > rocksTakenThisTurn;
+    }
+
+    /*a function that returns true if the we are allowed to take from pile p*/
+    public bool pileLockCheck(GameObject p)
+    {
+        // Pilelock == -1, anything goes
+        // Pilelock != -1 but the pile matches the locked pile, then we return true
+        return (PileLock == -1 || piles[PileLock] == p);
+    }
+
+    /*checks if all piles are empty, and returns true if that is the case*/
+    bool checkEmptyPiles()
+    {
+        for (int i=0; i<piles.Length; i++)
+        {
+            if (piles[i].GetComponent<Pile>().numRocks != 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    bool wincheck()
+    {
+        if (checkEmptyPiles() )
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    public void endTurn ()
+    {
+        if (wincheck())
+        {
+            Debug.Log("You win!");
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            Debug.Log("The game continues! Next move...");
+            moveNum += 1;
+            rocksTakenThisTurn = 0;
+        }
+    }
+
+    // Set this pile to be the only pile one may take from for the rest of the turn
+    public void setPileLock(GameObject p)
+    {
+        for (int i=0; i<piles.Length; i++)
+        {
+            if (piles[i] == p)
+            {
+                PileLock = i;
+                return;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Board/Pile.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pile : MonoBehaviour {
+
+    public int numRocks = 0;
+
+    public string GameType;
+
+    bool rockGrabbed = false;
+
+	// Use this for initialization
+	void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 mouse2world = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        Vector2 mouseCheck = new Vector2(mouse2world.x, mouse2world.y);
+
+        // On click, the player cursor must be over the Pile and there must be rocks here
+        if (Input.GetMouseButtonDown(0) && this.GetComponent<BoxCollider2D>().OverlapPoint(mouseCheck) && numRocks > 0)
+        {
+            rockGrabbed = true;
+        }
+
+        // Check for leftMouse is released
+        if (Input.GetMouseButtonUp(0) && rockGrabbed)
+        {
+            // If this was released over a player pile
+            if (pileCheck(mouseCheck))
+            {
+                this.removeRocks();
+                this.GetComponentInParent<Board>().PlayerPile.GetComponentInParent<PlayerPile>().addRocks();
+                this.GetComponentInParent<Board>().setPileLock(this.gameObject);
+            }
+
+            rockGrabbed = false;
+        }
+
+
+    }
+
+    bool pileCheck(Vector2 mc) 
+    {
+        return this.GetComponentInParent<Board>().pileCheck(mc, this.gameObject);
+    }
+
+    public void addRocks()
+    {
+        numRocks += 1;
+    }
+
+    public bool removeRocks()
+    {
+
+        if (numRocks > 0)
+        {
+            numRocks += -1;
+            Debug.Log("Rock removed, rocks remaining: " + numRocks.ToString());
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
